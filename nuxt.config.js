b8eb02e1a8fa716a09cf915c7d36abde30d92837
laci-grapes <jonas.laszlo@grapes.hu>
2021-05-03 14:40:24 +0200

    konfig módosítás

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,5 +38,16 @@ export default {
 
   content: {
     // Options
+  },
+
+  target: 'static',
+
+  generate: {
+    async routes () {
+      const { $content } = require('@nuxt/content')
+      const files = await $content().only(['path']).fetch()
+
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
   }
 }
